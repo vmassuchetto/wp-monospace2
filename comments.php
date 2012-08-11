87482a4ae5b4e2ca02d569acca4bf27b6a867f9f
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-08-11 18:40:05 -0300

    Adjusting comments.

--- comments.php
+++ comments.php
@@ -1,3 +1,4 @@
-<h3 id="comments" class="icon32 comments-icon32"><?php comments_number(__('Nenhum Comentário', 'monospace2'), __('1 Comentário', 'monospace2'), __('% Comentários', 'monospace2')); ?></h3>
-<ol class="commentlist"><?php wp_list_comments(array('avatar_size' => 48)); ?></ol>
+<h3 id="comments" class="icon32 comments-icon32"><?php comments_number(__('No Comment', 'monospace2'), __('1 Comment', 'monospace2'), __('% Comments', 'monospace2')); ?></h3>
 <?php comment_form(); ?>
+<ol class="commentlist"><?php wp_list_comments(array('avatar_size' => 48, 'reverse_top_level' => true, 'reverse_children' => false)); ?></ol>
+<?php paginate_comments_links(); ?>
